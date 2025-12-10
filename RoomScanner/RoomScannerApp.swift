1d978d10cfa47a915771330bbd320f4dd343d890
suhail <suhail.m@trezi.com>
2025-12-10 16:15:57 +0530

    Initial Commit

---
+++ RoomScanner/RoomScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomScannerApp.swift
+//  RoomScanner
+//
+//  Created by Abhishek trezi on 10/12/25.
+//
+
+import SwiftUI
+
+@main
+struct RoomScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RoomScannerTests/RoomScannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  RoomScannerTests.swift
+//  RoomScannerTests
+//
+//  Created by Abhishek trezi on 10/12/25.
+//
+
+import Testing
+@testable import RoomScanner
+
+struct RoomScannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
